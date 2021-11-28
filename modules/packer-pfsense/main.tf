f5806bc40d80604316c0c260df6fd2966ee8ed09
Jason Stanford <jps3@lehigh.edu>
2021-11-28 01:07:28 -0500

    Write settings to pkrvars auto file instead of env vars

--- modules/packer-pfsense/main.tf
+++ modules/packer-pfsense/main.tf
@@ -2,27 +2,35 @@ variable "vcenter_datacenter" {}
 variable "vcenter_cluster" {}
 variable "vcenter_datastore" {}
 variable "vcenter_host" {}
-variable "insecure_connection" {}
+variable "vs_insecure_connection" {}
 variable "iso_datastore" {}
 variable "iso_path" {}
 variable "iso_checksum" {}
 variable "vm_name" {}
 
-resource "null_resource" "packer_pfsense_build" {
-  provisioner "local-exec" {
-    environment = {
-      PKR_VAR_vcenter_datacenter="${var.vcenter_datacenter}"
-      PKR_VAR_vcenter_cluster="${var.vcenter_cluster}"
-      PKR_VAR_vcenter_datastore="${var.vcenter_datastore}"
-      PKR_VAR_vcenter_host="${var.vcenter_host}"
-      PKR_VAR_insecure_connection="${var.insecure_connection}"
-      PKR_VAR_iso_datastore="${var.iso_datastore}"
-      PKR_VAR_iso_path="${var.iso_path}"
-      PKR_VAR_iso_checksum="${var.iso_checksum}"
-    }
-    # TODO: If there's a failure this does not necessarily clean up!
-    command = "packer build -on-error=cleanup ."
-    working_dir = "${path.module}/packer"
-    interpreter = ["/bin/bash","-c"]
-  }
+resource "local_file" "pfsense_packer_auto_vars" {
+  sensitive_content = <<-EOF
+    vcenter_datacenter="${var.vcenter_datacenter}"
+    vcenter_cluster="${var.vcenter_cluster}"
+    vcenter_datastore="${var.vcenter_datastore}"
+    vcenter_host="${var.vcenter_host}"
+    vs_insecure_connection="${var.vs_insecure_connection}"
+    iso_datastore="${var.iso_datastore}"
+    iso_path="${var.iso_path}"
+    iso_checksum="${var.iso_checksum}"
+    vm_name="${var.vm_name}"
+  EOF
+  filename          = "${path.module}/packer/pfsense.auto.pkrvars.hcl"
+  file_permission   = "0600"
 }
+
+#resource "null_resource" "packer_pfsense_build" {
+#  provisioner "local-exec" {
+#    command     = "packer build -on-error=cleanup ."
+#    working_dir = "${path.module}/packer"
+#    interpreter = ["/bin/bash", "-c"]
+#  }
+#  depends_on = [
+#    local_file.pfsense_packer_auto_vars,
+#  ]
+#}
