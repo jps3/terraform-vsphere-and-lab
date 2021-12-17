a2da207701027c89f4190863da19cbd331096274
Jason Stanford <jps3@lehigh.edu>
2021-12-16 23:54:17 -0500

    Moved small *.tf file contents back into main.tf
      - It was getting a little unwieldy looking at the file listings in
        the editor, and the reason for separating them out in the first
        place was "legibility" but they are small in size.

--- main.tf
+++ main.tf
@@ -46,3 +46,39 @@ module "bastion" {
     resource.vsphere_folder.vm_folder
   ]
 }
+
+
+# ╭───────────────────────────────────╮
+# │    ┏━┓┏━╸┏━┓┏━┓╻ ╻┏━┓┏━╸┏━╸┏━┓    │
+# │    ┣┳┛┣╸ ┗━┓┃ ┃┃ ┃┣┳┛┃  ┣╸ ┗━┓    │
+# │    ╹┗╸┗━╸┗━┛┗━┛┗━┛╹┗╸┗━╸┗━╸┗━┛    │
+# ╰───────────────────────────────────╯
+
+resource "random_string" "lab_id" {
+  length  = 4
+  upper   = true
+  lower   = true
+  number  = true
+  special = false
+}
+
+resource "vsphere_folder" "vm_folder" {
+  path          = "${var.vm_folder}-${random_string.lab_id.result}"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+
+# ╭─────────────────────────────╮
+# │    ┏━┓╻ ╻╺┳╸┏━┓╻ ╻╺┳╸┏━┓    │
+# │    ┃ ┃┃ ┃ ┃ ┣━┛┃ ┃ ┃ ┗━┓    │
+# │    ┗━┛┗━┛ ╹ ╹  ┗━┛ ╹ ┗━┛    │
+# ╰─────────────────────────────╯
+
+output "gateway-ip" {
+  value = module.gateway.default-ip
+}
+
+output "bastion-ip" {
+  value = module.bastion.default-ip
+}
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "gateway-ip" {
-  value = module.gateway.default-ip
-}
-
-output "bastion-ip" {
-  value = module.bastion.default-ip
-}
